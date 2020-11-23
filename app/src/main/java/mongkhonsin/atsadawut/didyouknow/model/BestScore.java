0049bf4aca8f13aa53dfbbd2d8c69e6e26da6822
Atsadawut Mongkhonsin <atsada4188@gmail.com>
2020-11-23 21:49:40 +0700

    //23.11.2020 21:50 ครั้งที่ 7

---
+++ app/src/main/java/mongkhonsin/atsadawut/didyouknow/model/BestScore.java
@@ -0,0 +1,9 @@
+package mongkhonsin.atsadawut.didyouknow.model;
+
+public class BestScore {
+    public String userId;
+    public int foodCategoryBestScore;
+    public int animalCategoryBestScore;
+    public int artistCategoryBestScore;
+    public int generalCategoryBestScore;
+}
